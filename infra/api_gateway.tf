a73c879ac7e2c1d2bfc6f11e0aa3b1196ebdb28d
isaacweathersnet <isaac@isaacweathers.net>
2023-09-24 14:12:33 -0500

    Add in items path

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -30,3 +30,9 @@ resource "aws_api_gateway_deployment" "http-crud-tutorial-api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.http-crud-tutorial-api.id
   stage_name  = "dev"
 }
+
+resource "aws_api_gateway_resource" "usersResource" {
+  rest_api_id = aws_api_gateway_rest_api.http-crud-tutorial-api.id
+  parent_id   = aws_api_gateway_rest_api.http-crud-tutorial-api.root_resource_id # In this case, the parent id should the gateway root_resource_id.
+  path_part   = "items"
+}
