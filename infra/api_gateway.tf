5fe588bb8930bf2fb65ba5ba8767e0699a4fe73b
isaacweathersnet <isaac@isaacweathers.net>
2023-09-24 14:33:13 -0500

    Remove duplicate resource id

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -29,14 +29,14 @@ resource "aws_api_gateway_resource" "http-crud-tutorial-api_resource_id" {
   path_part   = "items/{id}"
 }
 
-resource "aws_api_gateway_method" "http-crud-tutorial-api_method" {
+resource "aws_api_gateway_method" "http-crud-tutorial-api_method_get_id" {
   rest_api_id   = aws_api_gateway_rest_api.http-crud-tutorial-api.id
   resource_id   = aws_api_gateway_resource.http-crud-tutorial-api_resource_id.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "http-crud-tutorial-api_method_put" {
+resource "aws_api_gateway_method" "http-crud-tutorial-api_method_put_id" {
   rest_api_id   = aws_api_gateway_rest_api.http-crud-tutorial-api.id
   resource_id   = aws_api_gateway_resource.http-crud-tutorial-api_resource_id.id
   http_method   = "PUT"
